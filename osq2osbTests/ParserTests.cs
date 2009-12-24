3bc574479e68929aadac5340ff70495c1f26aa93
unknown <strager@.(none)>
2009-12-24 10:49:24 -0500

    Moar tests lol

---
+++ osq2osbTests/ParserTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using osq2osb;
+using osq2osb.Parser;
+
+using NUnit.Framework;
+
+namespace osq2osb.Tests {
+    [TestFixture]
+    class ParserTests {
+        private void CheckTree(object expected, Parser.TreeNode.TokenNode tree) {
+            object[] array = expected as object[];
+
+            if(array != null) {
+                CheckTree(array[0], tree);
+
+                var tokenChildren = tree.TokenChildren;
+
+                for(int i = 1; i < array.Length; ++i) {
+                    CheckTree(array[i], tokenChildren[i - 1]);
+                }
+            } else {
+                Assert.AreEqual(expected, tree.Token.Value);
+            }
+        }
+
+        [Test]
+        public void MathEvaluation() {
+            var parser = new Parser.Parser();
+            var tree = parser.ExpressionToTokenNode("(2) + (17*2-30) * (5)+2 - (8/2)*4");
+
+            Assert.AreEqual(8, tree.Value);
+        }
+
+        [Test]
+        public void MathTree() {
+            var parser = new Parser.Parser();
+
+            CheckTree(new object[] {
+                    "+",
+                    2,
+                    3
+                },
+                parser.ExpressionToTokenNode("2 + 3"));
+
+            CheckTree(new object[] {
+                    "/",
+                    new object[] {
+                        "-",
+                        "rand",
+                        new object[] {
+                            "-",
+                            0.5
+                        }
+                    },
+                    4
+                },
+                parser.ExpressionToTokenNode("((rand()) - (-(0.5))) / ((4))"));
+        }
+    }
+}
--- osq2osbTests/TokenizerTests.cs
+++ osq2osbTests/TokenizerTests.cs
@@ -11,17 +11,30 @@ using NUnit.Framework;
 namespace osq2osb.Tests {
     [TestFixture]
     class TokenizerTests {
-        [Test]
-        public void MathExpression() {
-            var tokens = Tokenizer.Tokenize("( 2 + 4 ) * 7 + eval ( \"2 pi\" ) / func ( a , b )").ToList();
-
-            object[] expected = new object[] { "(", 2, "+", 4, ")", "*", 7, "+", "eval", "(", "2 pi", ")", "/", "func", "(", "a", ",", "b", ")" };
+        private void CheckTokenization(string input, object[] expected) {
+            var tokens = Tokenizer.Tokenize(input).ToList();
 
             Assert.AreEqual(expected.Length, tokens.Count());
 
             for(var i = 0; i < expected.Length; ++i) {
                 Assert.AreEqual(expected[i], tokens[i].Value);
             }
+        }
+
+        [Test]
+        public void MathExpression() {
+            CheckTokenization(
+                "( 2 + 4 ) * 7 + eval ( \"2 pi\" ) / func ( a , b )",
+                new object[] { "(", 2, "+", 4, ")", "*", 7, "+", "eval", "(", "2 pi", ")", "/", "func", "(", "a", ",", "b", ")" }
+            );
+        }
+
+        [Test]
+        public void Parentheses() {
+            CheckTokenization(
+                "((rand()) - (-(0.5))) / 4",
+                new object[] { "(", "(", "rand", "(", ")", ")", "-", "(", "-", "(", 0.5, ")", ")", ")", "/", 4 }
+            );
         }
 
         [Test]
