84f8d0d6d85a843c8ee0725d77dcbdb20cbe5dfb
strager <strager.nds@gmail.com>
2010-02-26 18:42:20 -0500

    Move tests from ParserTests to ExpressionRewriterTests.  Add parens exception tests.

--- osq2osbTests/ParserTests.cs
+++ osq2osbTests/ParserTests.cs
@@ -11,24 +11,6 @@ using NUnit.Framework;
 namespace osq2osb.Tests {
     [TestFixture]
     class ParserTests {
-        private static void CheckTree(object expected, Parser.TreeNode.TokenNode tree) {
-            object[] array = expected as object[];
-
-            if(array != null) {
-                CheckTree(array[0], tree);
-
-                var tokenChildren = tree.TokenChildren;
-
-                Assert.AreEqual(array.Length - 1, tokenChildren.Count);
-
-                for(int i = 1; i < array.Length; ++i) {
-                    CheckTree(array[i], tokenChildren[i - 1]);
-                }
-            } else {
-                Assert.AreEqual(expected, tree.Token.Value);
-            }
-        }
-
         private static void CheckParserOutput(string expected, string input) {
             var context = new ExecutionContext();
 
@@ -68,59 +50,6 @@ namespace osq2osb.Tests {
             tree = ExpressionToTokenNode("01:23:456 + 01:23");
 
             Assert.AreEqual((((1 * 60) + 23) * 1000 + 456) + ((1 * 60) + 23) * 1000, tree.Evaluate(context));
-        }
-
-        [Test]
-        public void MathTree() {
-            CheckTree(new object[] {
-                    "+",
-                    2,
-                    3
-                },
-                ExpressionToTokenNode("2 + 3"));
-
-            CheckTree(new object[] {
-                    "/",
-                    new object[] {
-                        "-",
-                        "rand",
-                        new object[] {
-                            "-",
-                            0.5
-                        }
-                    },
-                    4
-                },
-                ExpressionToTokenNode("((rand()) - (-(0.5))) / ((4))"));
-
-            CheckTree(new object[] {
-                    "a",
-                    new object[] {
-                        ",",
-                        "b",
-                        "c",
-                        "d"
-                    }
-                },
-                ExpressionToTokenNode("a(b, c, d)"));
-
-            CheckTree(new object[] {
-                "int",
-                new object[] {
-                    "+",
-                    new object[] {
-                        "*",
-                        new object[] {
-                            "-",
-                            "from",
-                            320
-                        },
-                        "mscale"
-                    },
-                    320
-                },
-                },
-                ExpressionToTokenNode("int((from - 320) * mscale + 320)"));
         }
 
         [Test]
