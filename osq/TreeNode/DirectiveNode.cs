3da172cbf59fb93eed17d244f942a12e8493ce7a
strager <strager.nds@gmail.com>
2010-03-20 13:42:53 -0400

    DirectiveNode documentation.

--- osq/TreeNode/DirectiveNode.cs
+++ osq/TreeNode/DirectiveNode.cs
@@ -6,34 +6,64 @@ using System.Text.RegularExpressions;
 using System.Reflection;
 
 namespace osq.TreeNode {
+    /// <summary>
+    /// Represents an osq directive.
+    /// </summary>
     public abstract class DirectiveNode : NodeBase {
+        /// <summary>
+        /// Contains information about a directive.
+        /// </summary>
         public class DirectiveInfo {
+            /// <summary>
+            /// Gets or sets the parser the directive is from.
+            /// </summary>
+            /// <value>The parser the directive is from.</value>
             public Parser Parser {
                 get;
                 set;
             }
 
+            /// <summary>
+            /// Gets or sets the name of the directive.
+            /// </summary>
+            /// <value>The name of the directive.</value>
             public string DirectiveName {
                 get;
                 set;
             }
 
+            /// <summary>
+            /// Gets or sets the location of the directive.
+            /// </summary>
+            /// <value>The location of the directive.</value>
             public Location Location {
                 get;
                 set;
             }
 
+            /// <summary>
+            /// Gets or sets a reader for the directive's parameters.
+            /// </summary>
+            /// <value>A reader for the directive's parameters.</value>
             public LocatedTextReaderWrapper ParametersReader {
                 get;
                 set;
             }
         }
 
+        /// <summary>
+        /// Gets or sets the name of the directive.
+        /// </summary>
+        /// <value>The name of the directive.</value>
         public string DirectiveName {
             get;
             private set;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DirectiveNode"/> class.
+        /// </summary>
+        /// <param name="info">The directive's information.</param>
         protected DirectiveNode(DirectiveInfo info) :
             base(info.Location) {
             DirectiveName = info.DirectiveName;
@@ -41,6 +71,13 @@ namespace osq.TreeNode {
 
         protected abstract bool EndsWith(NodeBase node);
 
+        /// <summary>
+        /// Reads a directive node.
+        /// </summary>
+        /// <param name="parser">The parser to read the directive node from.</param>
+        /// <returns>The directive node.</returns>
+        /// <exception cref="BadDataException">Read an invalid directive.</exception>
+        /// <exception cref="MissingDataException">Closing directive node was missing.</exception>
         public static DirectiveNode Create(Parser parser) {
             var startLocation = parser.InputReader.Location.Clone();
             string line = parser.InputReader.ReadLine();
@@ -70,6 +107,14 @@ namespace osq.TreeNode {
             throw new BadDataException("Unknown directive " + line, startLocation);
         }
 
+        /// <summary>
+        /// Parses a line containing a directive reference.
+        /// </summary>
+        /// <param name="parser">The parser.</param>
+        /// <param name="nameExpression">The regular expression to match the name of the directive.</param>
+        /// <param name="line">The line containing the directive.</param>
+        /// <param name="location">The location of the directive.</param>
+        /// <returns>A <see cref="DirectiveInfo"/> instance containing information about the directive, or <c>null</c> if the directive does not match the expression.</returns>
         private static DirectiveInfo ParseDirectiveLine(Parser parser, string nameExpression, string line, Location location) {
             Regex re = new Regex("^#(?<name>" + nameExpression + ")(\\s+(?<params>.*))?$", RegexOptions.ExplicitCapture);
 
@@ -95,18 +140,38 @@ namespace osq.TreeNode {
             };
         }
 
+        /// <summary>
+        /// Creates an instance of a directive.
+        /// </summary>
+        /// <param name="nodeType">Type of the directive node.</param>
+        /// <param name="info">The new instance's directive information.</param>
+        /// <returns>The new <see cref="DirectiveNode"/> instance.</returns>
         private static DirectiveNode CreateInstance(Type nodeType, DirectiveInfo info) {
             return Activator.CreateInstance(nodeType, info) as DirectiveNode;
         }
 
+        /// <summary>
+        /// Gets all the directive types available.
+        /// </summary>
+        /// <returns>Directive types.</returns>
         private static IEnumerable<Type> GetDirectiveTypes() {
             return GetDirectiveTypes(Assembly.GetCallingAssembly());
         }
 
+        /// <summary>
+        /// Gets the directive types of a given assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly in which to search for directive types.</param>
+        /// <returns>Directive types in <paramref name="assembly"/>.</returns>
         private static IEnumerable<Type> GetDirectiveTypes(Assembly assembly) {
             return assembly.GetTypes().Where((type) => !type.IsAbstract && type.IsSubclassOf(typeof(DirectiveNode)));
         }
 
+        /// <summary>
+        /// Reads the sub-nodes required by this node.
+        /// </summary>
+        /// <param name="parser">The parser to read sub-nodes from.</param>
+        /// <exception cref="MissingDataException">Closing directive node was not available.</exception>
         private void ReadSubNodes(Parser parser) {
             NodeBase curNode = this;
 
@@ -121,6 +186,12 @@ namespace osq.TreeNode {
             }
         }
 
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String"/> that represents this instance.
+        /// </returns>
         public override string ToString() {
             return "#" + DirectiveName;
         }
