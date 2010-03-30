c92be7e438adc1dce10cc938fc18e5cfce228bf3
strager <strager.nds@gmail.com>
2010-03-29 22:29:12 -0400

    Remove unused AddMany extension method.

--- osq/MyExtensions.cs
+++ osq/MyExtensions.cs
@@ -5,10 +5,5 @@ using System.Linq;
 
 namespace osq {
     public static class MyExtensions {
-        public static void AddMany<T>(this ICollection<T> self, IEnumerable<T> other) {
-            foreach(var otherItem in other) {
-                self.Add(otherItem);
-            }
-        }
     }
 }
